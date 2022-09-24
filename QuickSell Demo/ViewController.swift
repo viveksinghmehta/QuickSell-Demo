e66450e16fd10eedf726f6ae6120ce57ed8e0dce
Vivek Singh Mehta <viveksinghmehta.smr@gmail.com>
2022-09-24 15:06:35 +0530

    first commit

---
+++ QuickSell Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickSell Demo
+//
+//  Created by Vivek Singh Mehta on 24/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
